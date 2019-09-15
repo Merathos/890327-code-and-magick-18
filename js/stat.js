ccdbf9486626518c93105700dd86a0be06fffb94
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-09-15 12:49:14 +0300

    fixes

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,13 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_WIDTH = 420;
 var CLOUD_X = 150;
 var CLOUD_Y = 10;
+var CLOUD_X_WITH_GAP = CLOUD_X + GAP;
+var CLOUD_Y_WITH_GAP = CLOUD_Y + GAP;
 var CLOUD_COLOR = '#fff';
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 var CLOUD_FONT_COLOR = '#000';
 var CLOUD_FONT_SIZE = 16;
+var FONT_WITH_GAP = CLOUD_FONT_SIZE + GAP;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
@@ -30,27 +33,23 @@ var getRandomBlueColor = function () {
 
 window.renderStatistics = function (ctx, names, times) {
   var maxTime = getMaxElement(times);
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X_WITH_GAP, CLOUD_Y_WITH_GAP, CLOUD_SHADOW_COLOR);
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
   ctx.textBaseline = 'hanging';
   ctx.fillStyle = CLOUD_FONT_COLOR;
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, CLOUD_Y + GAP);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, CLOUD_Y + GAP + CLOUD_FONT_SIZE + GAP);
-  var currentBarHeight;
+  ctx.fillText('Ура вы победили!', CLOUD_X_WITH_GAP, CLOUD_Y_WITH_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X_WITH_GAP, CLOUD_Y_WITH_GAP + FONT_WITH_GAP);
   var currentX = CLOUD_X;
-  var currentY = CLOUD_Y + ((GAP + CLOUD_FONT_SIZE) * 2) + GAP + CLOUD_FONT_SIZE + BAR_HEIGHT;
-  for (var i = 0; i < names.length; i++) {
+  var currentY = CLOUD_Y + (FONT_WITH_GAP * 2) + FONT_WITH_GAP + BAR_HEIGHT;
+  for (var i = 0, currentBarHeight, finalYmargin; i < names.length; i++) {
     currentX += BAR_GAP;
     currentBarHeight = (times[i] * BAR_HEIGHT / maxTime);
-    if (names[i] === BAR_YOU_TEXT) {
-      ctx.fillStyle = BAR_YOU_COLOR;
-    } else {
-      ctx.fillStyle = getRandomBlueColor();
-    }
-    ctx.fillRect(currentX, currentY - BAR_HEIGHT + (BAR_HEIGHT - currentBarHeight), BAR_WIDTH, currentBarHeight);
+    finalYmargin = currentY - BAR_HEIGHT + (BAR_HEIGHT - currentBarHeight);
+    names[i] === BAR_YOU_TEXT ? ctx.fillStyle = BAR_YOU_COLOR : ctx.fillStyle = getRandomBlueColor();
+    ctx.fillRect(currentX, finalYmargin, BAR_WIDTH, currentBarHeight);
     ctx.fillStyle = CLOUD_FONT_COLOR;
     ctx.fillText(names[i], currentX, currentY + GAP);
-    ctx.fillText(Math.floor(times[i]), currentX, currentY - BAR_HEIGHT + (BAR_HEIGHT - currentBarHeight) - CLOUD_FONT_SIZE);
+    ctx.fillText(Math.floor(times[i]), currentX, finalYmargin - CLOUD_FONT_SIZE);
     currentX += BAR_WIDTH;
   }
 };

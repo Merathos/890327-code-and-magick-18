c803014c274c9225be0f8470ff9708da5219e6dd
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-09-20 13:56:40 +0300

    ff

--- js/setup.js
+++ js/setup.js
@@ -2,7 +2,6 @@
 
 var popUp = document.querySelector('.setup');
 var similarWizard = document.querySelector('.setup-similar');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 var names = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
@@ -30,9 +29,12 @@ var getRandomWizard = function () {
 };
 
 var renderWizards = function () {
-  getRandomWizard();
   var fragment = document.createDocumentFragment();
   var setupSimilarList = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
+
+  getRandomWizard();
+
   for (var i = 0; i < randomWizardList.length; i++) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.wizard-coat').style.fill = randomWizardList[i].coatColor;
